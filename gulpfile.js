a2d174d674a460a16af2fbf30a729fa8edfa4819
arlCode <adam@arlcode.com>
2019-04-10 17:41:44 -0700

    Configured the basic SASS rendering via gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 // Required Calls 
 const gulp = require('gulp'),
-    requireDir = require('require-dir'),
-    require('dotenv').config();
+    requireDir = require('require-dir');
 
 // Third-party Configuration settings
 const plugins = require('gulp-load-plugins') ({
@@ -10,5 +9,20 @@ const plugins = require('gulp-load-plugins') ({
     });
 
 const path = { // SASS folder sits outside the git repo default.
-    sassFolder = "sass/**/"
+    sassFolder: "sass/**/*",
+    cssFile: "style.min.css",
+    out: "out"
 }
+
+// This should be moved into its own seperate task folder.
+gulp.task('sass', function (error) {
+    return gulp.src(path.sassFolder)
+        .pipe(plugins.sass(plugins.sassOptions).on('error', plugins.sass.logError))
+        .pipe(plugins.concat(path.cssFile))
+        .pipe(plugins.autoprefixer(plugins.autoprefixerOptions))
+        .pipe(plugins.minifyCss())
+        .pipe(gulp.dest(path.out))
+});
+
+// Start build task. - This is going to be updated in the future.
+gulp.task('default', gulp.series('sass'));
